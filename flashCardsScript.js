e0dea7aa548b7ef1e62550d1ec7804b49e36e6fa
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-20 15:03:06 -0400

    Add files via upload
    End of Monday Sept 20

--- flashCardsScript.js
+++ flashCardsScript.js
@@ -1,3 +1,8 @@
+// function setup(){
+//     //let modal = '<div class="modal fade" id="deletingModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="ModalLabel">Delete card?</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">Are you sure you want to delete this card?</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button><button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick=deleteCard('+[i]+')>Confirm</button></div></div></div></div>';
+//     document.getElementById("modalPlacement").innerHTML = '<div class="modal fade" id="deletingModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="ModalLabel">Delete card?</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">Are you sure you want to delete this card?</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button><button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick=deleteCard('+[i]+')>Confirm</button></div></div></div></div>';
+// }
+
 function newCard(){
     document.getElementById('newTitleLabel').style.display='block';
     document.getElementById('newTitle').style.display='block';
@@ -7,21 +12,32 @@ function newCard(){
 }
 
 function createNewCard(){
-    let text = '<div class="accordion" id="accordionHeader"><div class="accordion-item"><h2 class="accordion-header" id="headingOne">';
-    const card0 = {title:document.getElementById("newTitle").value, desc:document.getElementById("newDesc").value};
-    cardList.push(card0);
-    for (var i = 0; i < cardList.length; i++) {
-        text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
-        text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close" onclick=deleteCard('+i+')></button>';
-        text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
+    if (document.getElementById("newTitle").value == ""){
+        alert("Please input the card title.");
     }
-    document.getElementById("cards").innerHTML = text;
+    else if (document.getElementById("newDesc").value == ""){
+        alert("Please input the card description.")
+    }
+    else {
+        let text = '<div class="accordion" id="accordionHeader"><div class="accordion-item"><h2 class="accordion-header" id="headingOne">';
+        const card0 = {title:document.getElementById("newTitle").value, desc:document.getElementById("newDesc").value};
+        cardList.push(card0);
+        for (var i = 0; i < cardList.length; i++) {
+            text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
+            text += '<button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#deletingModal">Delete card</button>';
+            //text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close" onclick=deleteCard('+i+')></button>';
+            text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
+        }
+        document.getElementById("cards").innerHTML = text;
 
-    document.getElementById('newTitleLabel').style.display='none';
-    document.getElementById('newTitle').style.display='none';
-    document.getElementById('newDescLabel').style.display='none';
-    document.getElementById('newDesc').style.display='none';
-    document.getElementById('submitForm').style.display='none';
+        document.getElementById("newTitle").value = "";
+        document.getElementById("newDesc").value = "";
+        document.getElementById('newTitleLabel').style.display='none';
+        document.getElementById('newTitle').style.display='none';
+        document.getElementById('newDescLabel').style.display='none';
+        document.getElementById('newDesc').style.display='none';
+        document.getElementById('submitForm').style.display='none';
+    }
 }
 
 function deleteCard(index){
@@ -30,7 +46,8 @@ function deleteCard(index){
     let text = '<div class="accordion" id="accordionHeader"><div class="accordion-item"><h2 class="accordion-header" id="headingOne">';
     for (var i = 0; i < cardList.length; i++) {
         text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
-        text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close"></button>';
+        text += '<button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#deletingModal">Delete card</button>';
+        //text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close" data-bs-toggle="modal" data-bs-target="#DeletionModal"></button>';
         text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
     }
     document.getElementById("cards").innerHTML = text;
