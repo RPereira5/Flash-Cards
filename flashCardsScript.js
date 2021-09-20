63d927ab9b31f48e276d0ad3018b474cf38c8681
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-20 11:54:41 -0400

    Add files via upload

---
+++ flashCardsScript.js
@@ -0,0 +1,37 @@
+function newCard(){
+    document.getElementById('newTitleLabel').style.display='block';
+    document.getElementById('newTitle').style.display='block';
+    document.getElementById('newDescLabel').style.display='block';
+    document.getElementById('newDesc').style.display='block';
+    document.getElementById('submitForm').style.display='block';
+}
+
+function createNewCard(){
+    let text = '<div class="accordion" id="accordionHeader"><div class="accordion-item"><h2 class="accordion-header" id="headingOne">';
+    const card0 = {title:document.getElementById("newTitle").value, desc:document.getElementById("newDesc").value};
+    cardList.push(card0);
+    for (var i = 0; i < cardList.length; i++) {
+        text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
+        text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close" onclick=deleteCard('+i+')></button>';
+        text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
+    }
+    document.getElementById("cards").innerHTML = text;
+
+    document.getElementById('newTitleLabel').style.display='none';
+    document.getElementById('newTitle').style.display='none';
+    document.getElementById('newDescLabel').style.display='none';
+    document.getElementById('newDesc').style.display='none';
+    document.getElementById('submitForm').style.display='none';
+}
+
+function deleteCard(index){
+    alert("Are you sure you want to delete this card?")
+    cardList.splice(index,1);
+    let text = '<div class="accordion" id="accordionHeader"><div class="accordion-item"><h2 class="accordion-header" id="headingOne">';
+    for (var i = 0; i < cardList.length; i++) {
+        text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
+        text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close"></button>';
+        text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
+    }
+    document.getElementById("cards").innerHTML = text;
+}
