b4a29c784a3ddf3c38752e8980e0c205eab538d4
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-21 10:08:41 -0400

    Add files via upload

--- flashCardsScript.js
+++ flashCardsScript.js
@@ -1,8 +1,3 @@
-// function setup(){
-//     //let modal = '<div class="modal fade" id="deletingModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="ModalLabel">Delete card?</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">Are you sure you want to delete this card?</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button><button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick=deleteCard('+[i]+')>Confirm</button></div></div></div></div>';
-//     document.getElementById("modalPlacement").innerHTML = '<div class="modal fade" id="deletingModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="ModalLabel">Delete card?</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">Are you sure you want to delete this card?</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button><button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick=deleteCard('+[i]+')>Confirm</button></div></div></div></div>';
-// }
-
 function newCard(){
     document.getElementById('newTitleLabel').style.display='block';
     document.getElementById('newTitle').style.display='block';
@@ -41,7 +36,6 @@ function createNewCard(){
 }
 
 function deleteCard(index){
-    alert("Are you sure you want to delete this card?")
     cardList.splice(index,1);
     let text = '<div class="accordion" id="accordionHeader"><div class="accordion-item"><h2 class="accordion-header" id="headingOne">';
     for (var i = 0; i < cardList.length; i++) {
