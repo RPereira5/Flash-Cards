0b4ab1c6bfe2683a08f10887d3790abc6da0b053
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-21 10:50:49 -0400

    Add files via upload
    Known bugs: Delete function always destroys the card at index 0

--- flashCardsScript.js
+++ flashCardsScript.js
@@ -19,14 +19,13 @@ function createNewCard(){
         cardList.push(card0);
         for (var i = 0; i < cardList.length; i++) {
             text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
-            text += '<button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#deletingModal">Delete card</button>';
-            //text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close" onclick=deleteCard('+i+')></button>';
+            text += '<button type="button" class="btn-close" id="close'+[i]+' aria-label="Close" data-bs-toggle="modal" data-bs-target="#deletingModal"></button>';
             text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
         }
         document.getElementById("cards").innerHTML = text;
 
-        document.getElementById("newTitle").value = "";
-        document.getElementById("newDesc").value = "";
+        document.getElementById('newTitle').value = "";
+        document.getElementById('newDesc').value = "";
         document.getElementById('newTitleLabel').style.display='none';
         document.getElementById('newTitle').style.display='none';
         document.getElementById('newDescLabel').style.display='none';
@@ -40,8 +39,7 @@ function deleteCard(index){
     let text = '<div class="accordion" id="accordionHeader"><div class="accordion-item"><h2 class="accordion-header" id="headingOne">';
     for (var i = 0; i < cardList.length; i++) {
         text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
-        text += '<button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#deletingModal">Delete card</button>';
-        //text += '<button type="button" class="btn-close" id="close'+[i]+'" aria-label="Close" data-bs-toggle="modal" data-bs-target="#DeletionModal"></button>';
+        text += '<button type="button" class="btn-close" id="close'+[i]+' aria-label="Close" data-bs-toggle="modal" data-bs-target="#deletingModal"></button>';
         text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
     }
     document.getElementById("cards").innerHTML = text;
