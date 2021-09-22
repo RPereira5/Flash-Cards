be00359e36679513ee05429261014d1d3ff3bfe2
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-22 14:59:31 -0400

    End of Wednesday Sept 22
    Today:
    - Completed dark/light theme toggle
    - Added a vector image to the "No Cards" message
    - Replaced the buttons and header with a navbar

--- flashCardsScript.js
+++ flashCardsScript.js
@@ -19,11 +19,12 @@ function createNewCard(){
     else {
         const card0 = {title:document.getElementById("newTitle").value, desc:document.getElementById("newDesc").value};
         cardList.push(card0);
-        let text = '<div class="accordion" id="accordionCards">';
+        let text = '<div class="accordion accordion-flush" id="accordionCards">';
         for (var i = 0; i < cardList.length; i++) {
-            text += '<div class="accordion-item"><h2 class="accordion-header" id="heading'+[i]+'"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
+            // text += '<div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample"><div class="accordion-body">Placeholder content for this accordion. This is the first item\'s accordion body.</div></div></div><div class="accordion-item"><h2 class="accordion-header" id="flush-headingTwo"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">Accordion Item #2</button></h2><div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample"><div class="accordion-body">Placeholder content for this accordion. This is the second item\'s accordion body. </div></div></div><div class="accordion-item"><h2 class="accordion-header" id="flush-headingThree"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">Accordion Item #3</button></h2><div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample"><div class="accordion-body">Placeholder content for this accordion.</div></div></div></div>';
+            text += '<div class="accordion-item"><h2 class="accordion-header" id="flush-heading'+[i]+'"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse'+[i]+'" aria-expanded="false" aria-controls="flush-collapse'+[i]+'">'+ cardList[i].title + '</button> </h2>';
             text += '<button type="button" class="btn-close" id="close'+[i]+' aria-label="Close" data-bs-toggle="modal" data-bs-target="#deletingModal"></button>';
-            text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
+            text += '<div id="flush-collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="flush-heading'+[i]+'" data-bs-parent="#accordionFlushHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
         }
         document.getElementById("cards").innerHTML = text;
 
