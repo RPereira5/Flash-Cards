0a4c7dcfcdd10f0ae511439ff1eb54d6f2026f11
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-22 14:35:33 -0400

    Testing for loop

--- flashCardsScript.js
+++ flashCardsScript.js
@@ -1,6 +1,7 @@
 function newCard(){
     document.getElementById('noCardsTitle').style.display = 'none';
     document.getElementById('noCards').style.display = 'none';
+    document.getElementById('cactus').style.display = 'none';
     document.getElementById('newTitleLabel').style.display = 'block';
     document.getElementById('newTitle').style.display = 'block';
     document.getElementById('newDescLabel').style.display = 'block';
@@ -50,6 +51,7 @@ function deleteCard(index){
         document.getElementById("cards").innerHTML = null;
         document.getElementById("noCardsTitle").style.display = 'block';
         document.getElementById("noCards").style.display = 'block'
+        document.getElementById("cactus").style.display = 'block';
     }
 }
 
@@ -58,31 +60,25 @@ function deleteArray(){
     let text = '<div class="accordion" id="accordionHeader">';
     for (var i = 0; i < cardList.length; i++) {
         text += '<div class="accordion-item"><h2 class="accordion-header" id="heading'+[i]+'><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+[i]+'" aria-expanded="false" aria-controls="collapse+">' + cardList[i].title + '</button> </h2>';
-        text += '<button type="button" class="btn-close" id="close'+[i]+' aria-label="Close" data-bs-toggle="modal" data-bs-target="#deletingModal"></button>';
+        text += '<button type="button" class="btn-close" id="close'+[i]+' aria-label="Close" data-bs-toggle="modal" data-bs-target="#deletingModal" title="Delete card"></button>';
         text += '<div id="collapse'+[i]+'" class="accordion-collapse collapse" aria-labelledby="heading'+[i]+'" data-bs-parent="#accordionHeader"> <div class="accordion-body">' + cardList[i].desc + '</div></div></div></div>';
     }
     document.getElementById("cards").innerHTML = text;
     document.getElementById("noCardsTitle").style.display = 'block';
     document.getElementById("noCards").style.display = 'block';
+    document.getElementById("cactus").style.display = 'block';
+    document.getElementById("resetButton").style.display = 'none';
 }
 
-// find @ /Users/rachel/Desktop/Code/Flash Cards/npm/bootstrap@5.0.2/js/src/dom/selector-engine.js:18:60
-
 function toggleTheme(){
-    // if (document.getElementById("mySheet").href == "flashCardsStyle.css"){
+    if (document.getElementById("toggleTheme").innerHTML == "Dark Mode"){
         document.getElementById("mySheet").href = "flashCardsDarkMode.css";
-        document.getElementById("toggleTheme").className = "btn btn-light";
         document.getElementById("toggleTheme").innerHTML = "Light Mode";
-        document.getElementById("cardMakerButton").className = "btn btn-info";
         document.getElementById("submitForm").className = "btn btn-info";
-        document.getElementById("resetButton").className = "btn btn-danger";
-    // }
-    /* else if (document.getElementById("mySheet").href == "flashCardsDarkMode.css"){
-        document.getElementById("mySheet").href = "flashCardsLightMode.css";
-        document.getElementById("toggleTheme").className = "btn btn-outline-dark";
+    }
+    else {
+        document.getElementById("mySheet").href = "flashCardsStyle.css";
         document.getElementById("toggleTheme").innerHTML = "Dark Mode";
-        document.getElementById("cardMakerButton").className = "btn btn-outline-info";
         document.getElementById("submitForm").className = "btn btn-outline-info";
-        document.getElementById("resetButton").className = "btn btn-outline-danger";
-    } */
+    }
 }
