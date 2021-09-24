ade8287c57a6fbb88e428c1fdb832fd864039dc3
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-24 11:33:04 -0400

    Delete cards by index fixed
    - TypeError still persists

--- flashCardsScript.js
+++ flashCardsScript.js
@@ -13,7 +13,7 @@ function drawCards(){
     let text = '<div class="accordion accordion-flush" id="accordionCards">';
     for (var i = 0; i < cardList.length; i++) {
         text += '<div class="row align-items-center"><div class="col-md-auto align-self-start"><br>';
-        text += '<button type="button" class="btn-close" id="close'+i+' aria-label="Close" data-bs-toggle="modal" data-bs-target="#deletingModal" onclick=showDeletingModal()></button></div>';
+        text += '<button type="button" class="btn-close" id="close'+i+' aria-label="Close" onclick=showDeletingModal('+i+') data-bs-toggle="modal" data-bs-target="#deletingModal"></button></div>';
         text += '<div class="col-11"><div class="accordion-item">';
         text += '<h2 class="accordion-header" id="flush-heading'+i+'">';
         text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse'+i+'" aria-expanded="false" aria-controls="flush-collapse'+i+'">'+cardList[i].title+'</button></h2>';
@@ -49,17 +49,14 @@ function createNewCard(){
 // only gives TypeError message on first click (i.e. first click on load)
 
 function showDeletingModal(cardIndex){
-    let modalText = '<div class="modal fade" id="deletingModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">';
-        modalText += '<div class="modal-dialog">';
-            modalText += '<div class="modal-content">';
-                modalText += '<div class="modal-header">';
+    let modalText = '<div class="modal fade" id="deletingModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
                     modalText += '<h5 class="modal-title" id="ModalLabel">Delete card?</h5>';
                     modalText += '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>';
                 modalText += '<div class="modal-body">Are you sure you want to <strong>delete this card</strong>? This action cannot be undone.</div>';         
                 modalText += '<div class="modal-footer">';
                     modalText += '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>';
                     modalText += '<button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick=deleteCard('+cardIndex+')>Delete</button>';
-    modalText += '</div></div></div></div></div>';
+    modalText += '</div></div></div></div>';
     document.getElementById('deletionModal').innerHTML = modalText;
 }
 
@@ -67,12 +64,12 @@ function deleteCard(index){
     cardList.splice(index,1);
     drawCards();
     if (cardList[0] == null){
-        document.getElementById("cards").innerHTML = null;
         document.getElementById("noCardsTitle").style.display = 'block';
         document.getElementById("noCards").style.display = 'block'
         document.getElementById("cactus").style.display = 'block';
         document.getElementById("resetButton").style.display = 'none';
     }
+    document.getElementById('deletionModal').innerHTML = "";
 }
 
 function deleteArray(){
