e0330cfdecbef6d81bbb349eb22590fe83d30459
RPereira5 <90706031+RPereira5@users.noreply.github.com>
2021-09-27 15:02:31 -0400

    End of Monday Sept 27
    Today:
    - learned JSON basics, began adding database functionality

--- flashCardsScript.js
+++ flashCardsScript.js
@@ -1,4 +1,5 @@
 function newCard(){
+    document.getElementById('cardForm').style.display = 'block';
     document.getElementById('noCardsTitle').style.display = 'none';
     document.getElementById('noCards').style.display = 'none';
     document.getElementById('cactus').style.display = 'none';
@@ -10,21 +11,39 @@ function newCard(){
 }
 
 function drawCards(){
-    let text = '<div class="accordion accordion-flush" id="accordionCards">';
-    console.log("Array length: "+cardList.length);
-    for (var i = 0; i < cardList.length; i++) {
-        text += '<div class="row align-items-center"><div class="col-md-auto align-self-start"><br>';
-        text += '<button type="button" class="btn-close" id="close'+i+'" aria-label="Close" onclick=showModal('+i+') data-bs-toggle="modal" data-bs-target="#deletingModal"></button></div>';
-        text += '<div class="col-11"><div class="accordion-item">';
-        text += '<h2 class="accordion-header" id="flush-heading'+i+'">';
-        text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse'+i+'" aria-expanded="false" aria-controls="flush-collapse'+i+'">'+cardList[i].title+'</button></h2>';
-        text += '<div id="flush-collapse'+i+'" class="accordion-collapse collapse" aria-labelledby="flush-heading'+i+'">';
-        text += '<div class="accordion-body">'+cardList[i].desc+'</div></div></div></div></div>';
+    fetch(/* own database will replace --> */'https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json => {
+        let text = '<div class="accordion accordion-flush" id="accordionCards">';
+        for (let i = 0; i < json.length; i++) {
+            text += '<div class="row justify-content-start"><div class="col-md-auto align-items-center"><br>';
+            text += '<button type="button" class="btn-close" id="close'+i+'" aria-label="Close" onclick=showModal('+i+') data-bs-toggle="modal" data-bs-target="#deletingModal"></button></div>';
+            text += '<div class="col-11"><div class="accordion-item">';
+            text += '<h2 class="accordion-header" id="flush-heading'+i+'">';
+            text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse'+i+'" aria-expanded="false" aria-controls="flush-collapse'+i+'">'+json[i].title+'</button></h2>';
+            text += '<div id="flush-collapse'+i+'" class="accordion-collapse collapse" aria-labelledby="flush-heading'+i+'">';
+            text += '<div class="accordion-body">'+json[i].desc+'</div></div></div></div></div>';
+        }
+        text += "</div>";
+        document.getElementById("cards").innerHTML = text;
     }
-    text += "</div>";
-    console.log("text: "+text);
-    document.getElementById("cards").innerHTML = text;
-}
+)};
+
+// ORIGINAL function drawCards(){
+//     let text = '<div class="accordion accordion-flush" id="accordionCards">';
+//     console.log("Array length: "+cardList.length);
+//     for (var i = 0; i < cardList.length; i++) {
+//         text += '<div class="row align-items-center"><div class="col-md-auto align-self-start"><br>';
+//         text += '<button type="button" class="btn-close" id="close'+i+'" aria-label="Close" onclick=showModal('+i+') data-bs-toggle="modal" data-bs-target="#deletingModal"></button></div>';
+//         text += '<div class="col-11"><div class="accordion-item">';
+//         text += '<h2 class="accordion-header" id="flush-heading'+i+'">';
+//         text += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse'+i+'" aria-expanded="false" aria-controls="flush-collapse'+i+'">'+cardList[i].title+'</button></h2>';
+//         text += '<div id="flush-collapse'+i+'" class="accordion-collapse collapse" aria-labelledby="flush-heading'+i+'">';
+//         text += '<div class="accordion-body">'+cardList[i].desc+'</div></div></div></div></div>';
+//     }
+//     text += "</div>";
+//     document.getElementById("cards").innerHTML = text;
+// }
 
 function createNewCard(){
     if (document.getElementById("newTitle").value == ""){
@@ -39,28 +58,22 @@ function createNewCard(){
         drawCards();
         document.getElementById('newTitle').value = "";
         document.getElementById('newDesc').value = "";
-        document.getElementById('newTitleLabel').style.display = 'none';
-        document.getElementById('newTitle').style.display = 'none';
-        document.getElementById('newDescLabel').style.display = 'none';
-        document.getElementById('newDesc').style.display = 'none';
-        document.getElementById('submitForm').style.display = 'none';
+        document.getElementById('cardForm').style.display = 'none';
         document.getElementById('resetButton').style.display = 'block';
     }
 }
 
 // only gives TypeError message on first click on load
-
 function showModal(cardIndex){
     var modalText = '<div class="modal fade" id="deletingModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
-                    modalText += '<h5 class="modal-title" id="ModalLabel">Delete card?</h5>';
-                    modalText += '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>';
-                modalText += '<div class="modal-body">Are you sure you want to <strong>delete this card</strong>? This action cannot be undone.</div>';         
-                modalText += '<div class="modal-footer">';
-                    modalText += '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>';
-                    modalText += '<button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick=deleteCard('+cardIndex+')>Delete</button>';
+    modalText += '<h5 class="modal-title" id="ModalLabel">Delete card?</h5>';
+    modalText += '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>';
+    modalText += '<div class="modal-body">Are you sure you want to <strong>delete this card</strong>? This action cannot be undone.</div>';         
+    modalText += '<div class="modal-footer">';
+    modalText += '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>';
+    modalText += '<button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick=deleteCard('+cardIndex+')>Delete</button>';
     modalText += '</div></div></div></div>';
     console.log("showModal: "+cardIndex);
-    console.log("modalText: "+document.getElementById('deletionModal').innerHTML);
     document.getElementById('deletionModal').innerHTML = modalText;
 }
 
